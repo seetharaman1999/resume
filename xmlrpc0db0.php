7843d48f72a2e5f4f428b623836af9d8cbd49e6b
seetharaman <c.seetharaman1999@gmail.com>
2019-05-30 23:32:15 +0530

    first commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://www.3jon.com/demo/themeforest/wp/verticademo/vertica_james/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
